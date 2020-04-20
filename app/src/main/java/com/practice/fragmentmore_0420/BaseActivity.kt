e5e9917f45c48de515f32236d3a857cfacbed6f0
jykim7760 <jykim7760@naver.com>
2020-04-20 19:38:06 +0900

    Initial commit

---
+++ app/src/main/java/com/practice/fragmentmore_0420/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.practice.fragmentmore_0420
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this;
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/com/practice/fragmentmore_0420/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.practice.fragmentmore_0420
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
+
+
+
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
